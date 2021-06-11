9755d3a4ab27181b44ddd1da835aa9855a6a7d5a
fahim-arif <fahim1.618555@gmail.com>
2021-06-11 13:20:44 +0600

    first commit to the project

---
+++ serverside/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use("/", (req, res, next) => {
+  res.send("Hello world");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
