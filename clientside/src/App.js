e9015fc53ed637290a54fce5905e7c745f625692
fahim-arif <fahim1.618555@gmail.com>
2021-08-08 12:24:04 +0600

    homepage complete, new footer and header added

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -4,12 +4,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/common/Header";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import HomeScreen from "./screens/HomeScreen";
 import TutorScreen from "./screens/TutorScreen";
 import ContactUs from "./screens/ContactUs";
 import Footer from "./components/common/Footer";
 import TuitionScreen from "./screens/TuitionScreen";
-import HomePageScreen from "./components/HomePageScreen";
+import HomePageScreen from "./screens/HomePageScreen";
 const App = () => {
   return (
     <Router>
